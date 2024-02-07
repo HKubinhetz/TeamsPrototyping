6768a3a741400f7d60c9934fdbb4ae5c6077aaae
Henrique Kubinha <rickubinhetz@gmail.com>
2024-02-07 16:40:20 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# This project represents the foundations on manipulating Microsoft Teams with python to automate repetitive tasks.
+
+# __________________________________________ Resources __________________________________________
+# 1) How to Send Microsoft Teams Messages with Python
+# https://www.datacamp.com/tutorial/how-to-send-microsoft-teams-messages-with-python
+#
+# 2) Report Automation with Python and PyMsTeams
+# https://medium.com/@miguel.arquez12/report-automation-with-python-and-pymsteams-b372188a515a
+# ___________________________________________________________________________________________
+
+# ------------------------------ Part One - Building Exercises ------------------------------
+
+
